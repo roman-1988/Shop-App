72fce8c965608f8c4595d4659703f1ffcff1f25d
roman krajewski <roman-1@inbox.ru>
2022-09-08 17:27:44 +0300

    created and used the hook useReducer

--- src/context.js
+++ src/context.js
@@ -1,11 +1,26 @@
-import {createContext} from "react";
+import {createContext, useReducer} from "react";
+import {reducer} from "./reducer";
 
 export const ShopContext = createContext();
 
+const initialState = {
+    goods: [],
+    loading: true,
+    order: [],
+    isBasketShow: false,
+    alertName: "",
+};
+
 export const ContextProvider = ({children}) => {
-    const value = {
-        example: "hello from context",
-    };
+    const [value, dispatch] = useReducer(reducer, initialState);
+
+    value.closeAlert = () => {
+        dispatch({type: "CLOSE_ALERT"})
+    }
+
+    value.removeFromBasket = (itemId) => {
+        dispatch({type: "REMOVE_FROM_BASKET", payload: {id: itemId}})
+    }
 
     return (
         <ShopContext.Provider value={value}>{children}</ShopContext.Provider>
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+export const reducer = (state, {type, payload}) => {
+    switch (type) {
+        case "REMOVE_FROM_BASKET":
+            return {
+                ...state,
+                order: state.order.filter((el) => el.id !== payload.id),
+            }
+        case "CLOSE_ALERT":
+            return {
+                ...state,
+                alertName: ""
+            }
+        default:
+            return state;
+    }
+}
